a5b8600ac05b0393d5aa37f80dbfc8743975b3a1
ejabno <jbak123@live.com>
2019-01-23 11:33:46 -0600

    Finished up code + documentation/comments. Added a .txt file for automated testing. Contains instructions on how to run the file.

--- calculator/stringcalc.py
+++ calculator/stringcalc.py
@@ -1,49 +1,108 @@
+"""
+Erickson Ureta
+erick.ureta95@gmail.com
+https://github.com/ejabno
+
+String calculator
+7Shifts technical interview challenge
+
+Running instructions:
+    1. The only need source files are:
+        - This file (stringcalc.py)
+        - A formatted .txt file containing test inputs (teststrings.txt)
+    2. Simply run this file on either the command line ("python stringcalc.py") or some other program
+"""
+
 def add(numbers):
+    """
+    Takes in an input string, parsing and searching for delimiters
+    Sums up any integers within the query
+    Excludes N > 1000 and throws a ValueError exception on N < 0
+    :param numbers: Input string to parse
+    :return: The parsed sum
+    """
     return_val = 0
-    numbers_len = len(numbers)
-    if numbers_len <= 0:
+
+    # Check for string length
+    if len(numbers) <= 0:
+        # If there's nothing in the string, there's nothing to return
         return 0
+
+    # Check for any custom delimiters
     if "//" in numbers:
+        # Query string has a custom delimiter
+        # This line divides the string between delimiter and actual query
         strip_array = numbers.strip("//").split("\n")
+        # Remove the leading "//"
         query_str = strip_array[1]
+        # Create a list of delimiters to search from
         delimiters = []
         for chr in strip_array[0]:
             if chr not in delimiters:
                 delimiters.append(chr)
+        # Then strip the query string of delimiters
         for delimiter in delimiters:
             query_str = query_str.replace(delimiter, ",")
         split_array = query_str.split(",")
     else:
+        # Query string has the normal delimiter
         split_array = numbers.split(",")
+
+    # Sum up the numbers
     for num in split_array:
         if len(num) <= 0:
+            # Sometimes the num array would contain
+            # empty strings from parsing multiple delimiters
+            # Therefore, ignore
             pass
         elif int(num) < 0:
+            # Exception on negatives
             raise ValueError(num)
         elif int(num) > 1000:
+            # Ignore N > 1000
             pass
         else:
             return_val += int(num)
+
     return return_val
 
 
 def runtests(filename):
-    with open(filename) as fopen:
-        fopen_lines = fopen.readlines()
+    """
+    Takes in a test file and automates testing
+    :param filename: The test file to take in test queries
+    """
     test_queries = []
-    while len(fopen_lines) > 0:
-        query_str = fopen_lines.pop(0)[:-1]
-        expected_result = int(fopen_lines.pop(0)[:-1])
-        test_queries.append((query_str, expected_result))
-    for test in test_queries:
+    # Parse testing file
+    with open(filename) as testfile:
+        curline = testfile.readline()
+        while curline is not "":
+            query_str = ""
+            # Parse the query string
+            while "equals" not in curline:
+                query_str += curline
+                curline = testfile.readline()
+            # Parse expected sum
+            exp_sum = int(curline.split(" ")[1])
+            # And add the query string + expected sum as a tuple
+            test_queries.append((query_str, exp_sum))
+            # Ignore "****" separator
+            testfile.readline()
+            curline = testfile.readline()
+    # Run all the tests
+    for query in test_queries:
+        print("==============")
+        print("Query string is \"%s\"" % query[0].replace("\n", "\\n"))
         try:
-            test_result = add(test[0])
-            if test_result != test[1]:
-                return False
-        except ValueError as e:
-            print("Exception caught on value %s" % e.args)
-    return True
+            result = add(query[0])
+            print("Expected result = %d, actual result = %d" % (query[1], result))
+            if result == query[1]:
+                print("Test passed")
+            else:
+                print("Test failed")
+        except ValueError as ve:
+            print("Exception caught on %d" % int(ve.args[0]))
 
 
-print(runtests("teststrings.txt"))
+runtests("teststrings.txt")
 
